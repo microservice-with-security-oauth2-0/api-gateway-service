3836cb4e81e97c3a158c12e8f49d50fea8183df3
kseshasai <kseshasai@SYS-66.mshome.net>
2020-08-12 14:44:35 +0530

    initial commit

---
+++ src/main/java/com/project/microservice/gateway/ApiGatewayServiceApplication.java
@@ -0,0 +1,17 @@
+package com.project.microservice.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient // It acts as a eureka client
+@EnableZuulProxy // Enable Zuul
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+
+}
